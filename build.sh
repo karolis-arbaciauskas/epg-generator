1fe9e7a9c05097e51b466bd0f89f3fa275d28ad7
Karolis Arbaciauskas <karolisarbaciauskas@Karoliss-MBP.lan>
2019-08-25 19:54:45 +0300

    move to src directory

--- build.sh
+++ build.sh
@@ -7,5 +7,5 @@ then
   apt -qq -y install zip
 fi
 
-dotnet restore
-dotnet lambda package --configuration release --framework netcoreapp2.1 --output-package bin/release/netcoreapp2.1/epg.zip
+dotnet restore src/aws-csharp.csproj
+dotnet lambda package --project-location src --configuration release --framework netcoreapp2.1 --output-package bin/release/netcoreapp2.1/epg.zip
